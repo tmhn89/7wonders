a57651b49a58757555548c996b6cb187197d951d
Ninh <tmhn89@gmail.com>
2018-11-14 17:06:47 +0200

    🔧  deployment config

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://tmhn89.github.io/7wonders/',
 
     /**
      * Source Maps
